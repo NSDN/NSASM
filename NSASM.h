8153c3307797c3e4dba9040f0c0b8bb4fc821a71
drzzm32 <drzzm32@gmail.com>
2018-01-29 17:44:18 +0800

    compile ok(

--- NSASM.h
+++ NSASM.h
@@ -1,1 +1,160 @@
-#pragma once
+#ifndef __NSASM_H_
+#define __NSASM_H_
+
+
+#include <functional>
+#include <vector>
+#include <stack>
+#include <map>
+
+namespace NSASM {
+
+	using namespace std;
+
+	class NSASM {
+
+	public:
+		const string version = "0.42";
+
+		enum Result {
+			RES_OK, RES_ERR, RES_ETC
+		};
+		
+		enum RegType {
+			REG_CHAR, REG_STR, REG_INT, REG_FLOAT, REG_CODE
+		};
+
+		class Register {
+		public:
+			Register() {
+				this->type = RegType::REG_INT;
+				this->readOnly = false;
+				this->gcFlag = false;
+				this->data.i = 0;
+			}
+			Register(const Register& reg) {
+				this->gcFlag = false;
+				type = reg.type;
+				strPtr = reg.strPtr;
+				readOnly = reg.readOnly;
+				switch (type) {
+				case RegType::REG_INT:
+					data.i = reg.data.i;
+					break;
+				case RegType::REG_CHAR:
+					data.c = reg.data.c;
+					break;
+				case RegType::REG_FLOAT:
+					data.f = reg.data.f;
+					break;
+				case RegType::REG_STR:
+					data.s = reg.data.s;
+					break;
+				case RegType::REG_CODE:
+					data.s = reg.data.s;
+					break;
+				}
+			}
+			~Register() {
+				if (this->type == RegType::REG_STR || this->type == RegType::REG_CODE)
+					this->data.s.clear();
+			}
+
+		public:
+			Register& operator=(const Register& reg) {
+				this->gcFlag = false;
+				type = reg.type;
+				strPtr = reg.strPtr;
+				readOnly = reg.readOnly;
+				switch (type) {
+				case RegType::REG_INT:
+					data.i = reg.data.i;
+					break;
+				case RegType::REG_CHAR:
+					data.c = reg.data.c;
+					break;
+				case RegType::REG_FLOAT:
+					data.f = reg.data.f;
+					break;
+				case RegType::REG_STR:
+					data.s = reg.data.s;
+					break;
+				case RegType::REG_CODE:
+					data.s = reg.data.s;
+					break;
+				}
+				return *this;
+			}
+
+		public:
+			RegType type;
+			union Data {
+			public:
+				Data() { i = 0; }
+				~Data() {}
+			public:
+				int i; char c;
+				float f; string s;
+
+			} data;
+			int strPtr = 0;
+			bool readOnly;
+			bool gcFlag;
+		};
+
+		typedef function<Result(Register*, Register*)> Operator;
+
+		Result execute(string var);
+		Register* run();
+		void call(string segName);
+
+		NSASM(int heapSize, int stackSize, int regCnt, map<string, string> code);
+		~NSASM();
+
+	protected:
+		vector<Register> regGroup;
+		map<string, Operator> funcList;
+
+		Register* eval(Register* reg);
+		void loadFuncList();
+
+	private:
+		enum WordType {
+			WD_REG, WD_CHAR, WD_STR, WD_INT,
+			WD_FLOAT, WD_VAR, WD_TAG, WD_SEG,
+			WD_CODE
+		};
+
+		map<string, Register> heapManager;
+		stack<Register> stackManager;
+		int heapSize, stackSize;
+		Register stateReg;
+		Register prevDstReg;
+
+		stack<int> backupReg;
+		int progSeg, tmpSeg;
+		int progCnt, tmpCnt;
+
+		map<string, vector<string>> code;
+
+		bool verifyBound(string var, char left, char right);
+		bool verifyWord(string var, WordType type);
+		Register* getRegister(string var);
+		vector<string> convToArray(string var);
+		Result appendCode(map<string, string> code);
+		void copyRegGroup(NSASM super);
+
+		Result calc(int* dst, int src, char type);
+		Result calc(char* dst, char src, char type);
+		Result calc(float* dst, float src, char type);
+		Result calc(Register* dst, int src, char type);
+		Result calc(Register* dst, Register* src, char type);
+
+		NSASM(NSASM super, map<string, string> code);
+
+	};
+
+}
+
+
+#endif
--- Util.h
+++ Util.h
@@ -1,9 +1,15 @@
-#pragma once
+#ifndef __UTIL_H_
+#define __UTIL_H_
 
+
+#include <functional>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 #include <map>
+
+#define nulstr ""
 
 namespace NSASM {
 
@@ -11,22 +17,29 @@ namespace NSASM {
 
 	class Util {
 
-		typedef void(*Printer)(string);
-		typedef string(*Scanner)();
-		typedef string(*FileReader)(string);
-
 	public:
-		static Printer Output;
-		static Scanner Input;
-		static FileReader FileInput;
-
-		Util() {
-			Output = NULL;
-			Input = NULL;
-			FileInput = NULL;
+		static Util& I() {
+			static Util inst;
+			return inst;
 		}
 
-	private:
+		typedef function<void(string)> Printer;
+		typedef function<string(void)> Scanner;
+		typedef function<string(string)> FileReader;
+
+		Printer Output;
+		Scanner Input;
+		FileReader FileInput;
+
+		Util() {
+			Output = [](string str) -> void { cout << str; };
+			Input = [](void) -> string { string s = ""; cin >> s; return s; };
+			FileInput = [](string path) -> string {
+				return "";
+			};
+		}
+
+	public:
 		static inline void replace(string& var, string src, string dst) {
 			size_t pos, begin = -1;
 			while (pos = var.find(src) != var.npos) {
@@ -44,157 +57,28 @@ namespace NSASM {
 		}
 
 	public:
-		static void print(string value) {
-			if (Output != NULL) Output(value);
-		}
-
-		static string scan() {
-			if (Input != NULL) return Input();
-			return "";
-		}
-
-		static void cleanSymbol(string& var, string symbol, string trash) {
-			cycleReplace(var, symbol + trash, symbol);
-			cycleReplace(var, trash + symbol, symbol);
-		}
-
-		static void cleanSymbol(string& var, string symbol, string trashA, string trashB) {
-			cycleReplace(var, symbol + trashA, symbol);
-			cycleReplace(var, symbol + trashB, symbol);
-			cycleReplace(var, trashA + symbol, symbol);
-			cycleReplace(var, trashB + symbol, symbol);
-		}
-
-		static string formatLine(string var) {
-			if (var.length() == 0) return "";
-			
-			cycleReplace(var, "\r", "");
-			while (var[0] == '\t' || var[0] == ' ') {
-				var = var.substr(1);
-				if (var.length() == 0) return "";
-			}
-
-			string left, right; size_t pos;
-			if (pos = var.find("\'") != var.npos) {
-				left = var.substr(0, pos);
-				right = var.substr(left.length());
-			} else if (pos = var.find("\"") != var.npos) {
-				left = var.substr(0, pos);
-				right = var.substr(left.length());
-				if (pos = right.substr(1).find("\"") != right.npos && (pos + 1) < right.length()) {
-					if (right.find("*", pos + 1) != right.npos) {
-						cleanSymbol(right, "*", "\t", " ");
-					}
-				}
-			} else {
-				left = var;
-				right = "";
-			}
-			cycleReplace(left, "\t", " "); cycleReplace(left, "  ", " ");
-			cleanSymbol(left, ",", " "); cleanSymbol(left, "=", " ");
-			cleanSymbol(left, "{", "\t", " "); cleanSymbol(left, "}", "\t", " ");
-			cleanSymbol(left, "(", "\t", " "); cleanSymbol(left, ")", "\t", " ");
-
-			return left + right;
-		}
-
-		static string formatCode(string var) {
-			string buf = "", tmp = "";
-			stringstream reader(var);
-			while (!reader.eof()) {
-				getline(reader, tmp);
-				buf += formatLine(tmp + "\n");
-			}
-			cycleReplace(buf, "\n\n", "\n");
-			reader.clear();
-			return buf;
-		}
-
-		static void repairBrackets(string& var, string left, string right) {
-			cycleReplace(var, "\n" + left, left);
-			replace(var, left, left + "\n");
-			replace(var, right, "\n" + right);
-			cycleReplace(var, "\n\n", "\n");
-			cycleReplace(var, left + " ", left);
-			cycleReplace(var, " \n" + right, "\n" + right);
-		}
-
-		static void encodeLambda(string& var) {
-			replace(var, "\n", "\f");
-		}
-
-		static void decodeLambda(string var) {
-			replace(var, "\f", "\n");
-		}
-
-		static void formatString(string var) {
-			replace(var, "\\\"", "\"");
-			replace(var, "\\\'", "\'");
-			replace(var, "\\\\", "\\");
-			replace(var, "\\n", "\n");
-			replace(var, "\\t", "\t");
-		}
-
-		static string formatLambda(string var) {
-			const int IDLE = 0, RUN = 1, DONE = 2;
-			int state = IDLE, count = 0, begin = 0, end = 0;
-			string a = "", b = "", c = "";
-
-			for (int i = 0; i < var.length; i++) {
-				switch (state) {
-				case IDLE:
-					count = begin = end = 0;
-					if (var[i] == '(')
-					{
-						begin = i;
-						count += 1;
-						state = RUN;
-					}
-					break;
-				case RUN:
-					if (var[i] == '(')
-						count += 1;
-					else if (var[i] == ')')
-						count -= 1;
-					if (count == 0)
-					{
-						end = i;
-						state = DONE;
-					}
-					break;
-				case DONE:
-					a = var.substr(0, begin);
-					b = var.substr(begin, end - begin + 1);
-					c = var.substr(end + 1);
-					encodeLambda(b);
-					var = a + b + c;
-					state = IDLE;
-					break;
-				default:
-					return var;
-				}
-			}
-		
-			return var;
-		}
-
-		static map<string, string> getSegments(string var) {
-			map<string, string> segs;
-			string pub = "", buf = var;
-
-			buf = formatCode(buf);
-			repairBrackets(buf, "{", "}");
-			repairBrackets(buf, "(", ")");
-			buf = formatCode(buf);
-
-			buf = formatLambda(buf);
-
-			stringstream reader(buf);
-			string head, body = "", tmp;
-
-			return segs;
-		}
+		template<typename T>
+		static void print(T value);
+		static string scan();
+		static void cleanSymbol(string& var, string symbol, string trash);
+		static void cleanSymbol(string& var, string symbol, string trashA, string trashB);
+		static string formatLine(string var);
+		static string formatCode(string var);
+		static void repairBrackets(string& var, string left, string right);
+		static void encodeLambda(string& var);
+		static void decodeLambda(string& var);
+		static void formatString(string& var);
+		static string formatLambda(string var);
+		static map<string, string> getSegments(string var);
+		static string getSegment(string var, string head);
+		static string read(string path);
+		static void run(string path);
+		static void execute(string str);
+		static void interactive();
 
 	};
 
 }
+
+
+#endif
