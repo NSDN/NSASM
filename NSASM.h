1af3a66e2ccdfd268664a7c9811ef033493de9de
drzzm32 <drzzm32@gmail.com>
2019-05-20 22:20:14 +0800

    update version

--- NSASM.h
+++ NSASM.h
@@ -16,7 +16,7 @@ namespace NSASM {
 	class NSASM {
 
 	public:
-		static string ver() { return "0.54"; }
+		static string ver() { return "0.54-inline"; }
 
 		enum Result {
 			RES_OK, RES_ERR, RES_ETC
